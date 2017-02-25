5cf112daf9d2b102272fb98d50dbdf156c20583b
Imran Mondal <hello@imranmondal.com>
2017-02-25 16:00:26 +0600

    Homepage markup and styling complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-//Custom Scripts
+/* ----------------------------------/
+/ 			Custom Scripts 			 /
+/-----------------------------------*/
+
+//Navbar scroll color
+$(document).ready(function() {
+	$(window).scroll(function() {
+		
+		// When user scroll more than 60, add .navbar-transparent-onscroll
+		if($(document).scrollTop() > 70) {
+			$(".navbar-transparent").addClass("navbar-transparent-onscroll");
+		} else {
+			$(".navbar-transparent").removeClass("navbar-transparent-onscroll");
+		}
+
+	});
+	
+});
