e70967d3c1cc950974d922229fc35d77db088873
Imran Mondal <hello@imranmondal.com>
2017-03-15 22:43:06 +0600

    Demo content replaced

--- js/nav-scroll.js
+++ js/nav-scroll.js
@@ -1,17 +1,17 @@
-var $ 			= require("jquery");
+var $ = require("jquery");
 //Navbar scroll color
 var navScroll = $(document).ready(function() {
-		
-		$(window).scroll(function() {
-		// When user scroll more than 60, add .navbar-transparent-onscroll
-		if($(document).scrollTop() > 70) {
-			$(".navbar-transparent").addClass("navbar-transparent-onscroll");
-		} else {
-			$(".navbar-transparent").removeClass("navbar-transparent-onscroll");
-		}
 
-	});
-	
+    $(window).scroll(function() {
+        // When user scroll more than 60, add .navbar-transparent-onscroll
+        if ($(document).scrollTop() > 70) {
+            $(".navbar-transparent").addClass("navbar-transparent-onscroll");
+        } else {
+            $(".navbar-transparent").removeClass("navbar-transparent-onscroll");
+        }
+
+    });
+
 });
 
 module.exports = navScroll;
